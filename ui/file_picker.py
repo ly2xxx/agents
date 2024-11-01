6294c317652d54495cbc5012b46e429a9601221a
ly2xxx <ly2xxx@hotmail.com>
2024-11-01 23:19:30 +0000

    move file picker to side bar

--- ui/file_picker.py
+++ ui/file_picker.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
 def render_file_picker(support_types):
-    uploaded_files = []
-    num_files = st.number_input("Pick your file(s) - files for Retrieval Augmented Query", min_value=1, value=1, step=1)
-    for i in range(num_files):
-        file = st.file_uploader(f"Choose file {i+1}", type=support_types, key=f"file_{i}")
-        if file:
-            uploaded_files.append(file)
+    with st.sidebar:
+        uploaded_files = []
+        num_files = st.number_input("Pick your file(s) - files for Retrieval Augmented Query", min_value=1, value=1, step=1, key="num_files")
+        for i in range(num_files):
+            file = st.file_uploader(f"Choose file {i+1}", type=support_types, key=f"file_{i}")
+            if file:
+                uploaded_files.append(file)
     return uploaded_files
